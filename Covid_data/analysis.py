0652bb6a37bf4cf9d3c02429c1b99178f492f3b6
sona444 <sonakshi2500@gmail.com>
2021-10-20 17:02:03 +0530

    sentiments 1

---
+++ Covid_data/analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.downloader.download('vader_lexicon')
+file="D:\SentimentAnalysis\Covid_data\covid-data.xlsx"
+
+dfs=pd.read_excel(file)
+print(dfs)
+sid=SentimentIntensityAnalyzer()
+for index,data in dfs.iterrows():
+    data=data[0].split('.  ')
+    for sa in data:
+        sa=sid.polarity_scores(str(data))
+        for k in sa:
+            print(k,sa[k])
